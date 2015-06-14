0f888ba49fb20d292044ec85194356e91b8e1979
unkei <unkei@yahoo.co.jp>
2015-06-14 10:45:17 +0900

    remvoed socket.io dependency

--- backend.js
+++ backend.js
@@ -2,24 +2,45 @@ var port = process.env.PORT || 8000;
 var http = require('http');
 var express = require('express');
 var app = express();
-var server = http.createServer(app).listen(port);
-var io = require('socket.io').listen(server);
+var WebSocketServer = require('ws').Server;
 
 app.use(express.static(__dirname + '/www'));
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/www/index.html');
 });
 
+var server = http.createServer(app);
 server.listen(port, function() {
     console.log("Listening on port " + port + "...");
 });
 
-io.on('connection', function(socket) {
+var wss = new WebSocketServer({server: server});
+var connections = [];
+
+wss.on('connection', function(socket) {
+    connections.push(socket);
+    console.log("new socket connected :" + connections.length);
+    socket.on('close', function () {
+        connections = connections.filter(function (conn, i) {
+            return (conn === socket) ? false : true;
+        });
+        console.log("socket closed :" + connections.length);
+    });
+
     socket.on('message', function(message) {
-        socket.broadcast.emit('message', message);
+        broadcast(socket, message);
     });
  
     socket.on('disconnect', function() {
-    socket.broadcast.emit('user disconnected');
+        broadcast(socket, '{"type": "user disconnected"}');
     });
 });
+
+function broadcast(ws, msg) {
+    console.log("broadcast(" + msg + ")");
+    connections.forEach(function (con, i) {
+        if (ws !== con) {
+            con.send(msg);
+        }
+    });
+}
